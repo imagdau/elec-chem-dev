ede8b6d3c5bd036c15cc3df3fb691dcd89978d74
Ioan-Bogdan Magdau <i.b.magdau@gmail.com>
2024-05-01 22:57:22 +0100

    work in progress

--- elecChemDev/search.py
+++ elecChemDev/search.py
@@ -1,8 +1,9 @@
 from mp_api.client import MPRester
 import warnings
+from pandas import DataFrame
 warnings.filterwarnings("ignore")
 
-def sorted_elem_list(elements):
+def get_elem_comb(elements):
     elements.sort(key=lambda element: element._atomic_mass, reverse=True)
     return "".join([el.symbol for el in elements])
 
@@ -12,15 +13,14 @@ class elyte(MPRester):
             API_KEY = keyFile.readline().rstrip()
         super().__init__(API_KEY)
         self.docs = self.molecules.jcesr.search(nelements=(0,1000))
-        self.dict_by_elements = dict()
         
         
     def list_elem_combs(self):
         elem_combs = []
         for doc in self.docs:
-            elem_combs.append(sorted_elem_list(doc.elements))
+            elem_combs.append(get_elem_comb(doc.elements))
         return sorted(list(set(elem_combs)))
-            
+
             
 # Natoms = len(doc.molecule._sites)
 # if el_str in self.dict_by_elements:
