91b28234becdb0151ee25e1c1faf59a051f6f0ca
Ioan-Bogdan Magdau <i.b.magdau@gmail.com>
2024-05-02 00:42:49 +0100

    working for liquid electrolyte

--- build/lib/elecChemDev/view.py
+++
@@ -1,8 +0,0 @@
-import nglview as ngl
-from IPython.display import SVG
-
-def view_atoms(config):
-    view = ngl.show_ase(config)
-    view.add_representation(repr_type='unitcell')
-    view._set_size(w='100%', h='500px')
-    return view
