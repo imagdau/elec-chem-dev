e2f02cab6b728ccf1e351fa1b6ad556792f1651a
Ioan-Bogdan Magdau <nim92@ioan-mac.ncl.ac.uk>
2024-09-24 14:29:30 +0100

    implemented cathode building

--- elecChemDev/search.py
+++ elecChemDev/search.py
@@ -10,12 +10,8 @@ warnings.filterwarnings("ignore")
 pd.set_option('display.max_rows', 100)
 
 class elyte(MPRester):
-    def __init__(self, path=''):
-        if path:
-            fkey = path
-        else:
-            fkey = "api.key"
-        with open(fkey, "r") as keyFile:
+    def __init__(self, path='api.key'):
+        with open(path, "r") as keyFile:
             API_KEY = keyFile.readline().rstrip()
         super().__init__(API_KEY)
         self.docs = self.molecules.jcesr.search(nelements=(0,1000))
@@ -55,3 +51,36 @@ class elyte(MPRester):
     def show_pics(self, hashes):
         svgs = self.get_molecs(hashes, field='svg')
         display(*[SVG(svg) for svg in svgs])
+
+
+class elode(MPRester):
+    def __init__(self, path='api.key', elem=['Li', 'Ni', 'Mn', 'Co', 'O']):
+        with open(path, "r") as keyFile:
+            API_KEY = keyFile.readline().rstrip()
+        super().__init__(API_KEY)
+        self.docs = self.materials.search(elements=elem)
+        col = {
+            'elemComb': [],
+            'natoms': [],
+            'formula': [],
+            'mass': [],
+            'charge': [],
+            'mpid': [],
+            'hash': [],
+            'Atoms': []
+        }
+        for doc in self.docs:
+            at = Atoms(AseAtomsAdaptor.get_atoms(doc.structure))
+            ea.hash_atoms([at])
+            col['elemComb'].append("".join(sorted(set(at.get_chemical_symbols()))))
+            col['natoms'].append(len(at))
+            col['formula'].append(at.get_chemical_formula())
+            col['mass'].append(at.get_masses().sum())
+            col['charge'].append(doc.structure._charge)
+            col['mpid'].append(doc.material_id) #for some reason, not unique
+            col['hash'].append(at.info['uid'])
+            col['Atoms'].append(at)
+        self.df = pd.DataFrame(col).sort_values(by=['elemComb', 'natoms', 'mass', 'charge']).drop_duplicates(subset=['hash']).set_index('hash')
+
+    def get_material(self, hash, field):
+        return self.df.loc[hash][field]
