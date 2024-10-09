5d3d99e46b1500aa64aa7c76d31099a174d65631
Ioan-Bogdan Magdau <nim92@ioan-mac.ncl.ac.uk>
2024-10-09 17:01:29 +0100

    developed models for the interface

--- elecChemDev/build.py
+++ elecChemDev/build.py
@@ -4,6 +4,7 @@ from ase.build.attach import attach_randomly
 import ase.units
 from aseMolec import anaAtoms as aa
 from ase import Atoms
+import numpy as np
 
 def attach_randomly_attempts(at1, at2, distance, Nattempts=100):
     attempt = 0
@@ -16,10 +17,10 @@ def attach_randomly_attempts(at1, at2, distance, Nattempts=100):
             pass
     raise RuntimeError('Attach did not converge after '+str(attempt)+' attempts. Consider increasing Nattempts or Volume!')
 
-def pack_mols(db, Nmols, dens=1.0, cell=[1,1,1], distance=2.0, Nattempts=200, fct=1):
+def pack_mols(mols, Nmols, dens=1.0, cell=[1,1,1], distance=2.0, Nattempts=200, fct=1):
     #compute total mass
     M = 0
-    for at, N in zip(db, Nmols):
+    for at, N in zip(mols, Nmols):
         M += at.get_masses().sum()*N
 
     #rescale volume to match target density
@@ -31,7 +32,7 @@ def pack_mols(db, Nmols, dens=1.0, cell=[1,1,1], distance=2.0, Nattempts=200, fc
 
     #shuffle molecule collection
     col = []
-    for at, N in zip(db, Nmols):
+    for at, N in zip(mols, Nmols):
         for i in range(N):
             col.append(at)
     random.shuffle(col)
@@ -44,4 +45,33 @@ def pack_mols(db, Nmols, dens=1.0, cell=[1,1,1], distance=2.0, Nattempts=200, fc
     aa.wrap_molecs([config], fct)
     config.center()
     
+    return config
+
+def fill_mols(config, mols, Nratio, Vacuum, dens=1.0, distance=2.0, Nattempts=200, fct=1):
+
+    Nstart = len(config)
+    #compute total mass to add per round
+    M = 0
+    for at, N in zip(mols, Nratio):
+        M += at.get_masses().sum()*N
+
+    #rescale volume to match target density
+    densfact = (ase.units.m/1.0e2)**3/ase.units.mol
+    Nscale = int(Vacuum*dens/densfact/M)
+
+    #shuffle molecule collection
+    col = []
+    for at, N in zip(mols, Nratio):
+        for i in range(N*Nscale):
+            col.append(at)
+    random.shuffle(col)
+    
+    #add molecules one by one
+    for at in tqdm(col):
+        config = attach_randomly_attempts(config, at, distance=distance, Nattempts=Nattempts)
+
+    mask = [True]*len(config)
+    for i in range(Nstart):
+        mask[i] = False
+    aa.wrap_molecs_partial([config], fct, mask=mask)
     return config
